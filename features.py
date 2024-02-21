e1d826a87bd9d77849983c038a73d5c75eda276a
Mohan Tirumalasetti <tirumalasettimohansrisai@gmail.com>
2024-02-22 02:53:15 +0530

    Adding Favicon function and python venv file

--- features.py
+++ features.py
@@ -3,13 +3,46 @@ import re
 import ssl 
 import socket
 from datetime import datetime
+import whois
+from bs4 import BeautifulSoup
+import requests
+from urllib.parse import urlparse
 
 class FeaturesExtractor:
     features = []
 
     def __init__(self, url):
         self.url = url
-        self.domain = str(re.findall(".*://?([^/]+)",self.url)[0])
+        self.domain = ""
+        self.url_parse = ""
+        self.whois_res = ""
+        self.soup = ""
+
+        try:
+            self.url_parse = urlparse(url)
+            self.domain = self.url_parse.netloc
+        except:
+            pass
+
+
+        try:
+            headers = { 'Accept-Language' : "en-GB,en;q=0.7",
+            'User-Agent':"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"}
+
+
+
+            self.response = requests.get(url, headers=headers)
+            self.soup = BeautifulSoup(self.response.text, "html.parser")
+        except:
+            pass
+
+
+        try:
+            self.whois_res = whois.whois(self.domain)
+        except:
+            pass
+
+
         self.features.append(self.having_IP_Address())
         self.features.append(self.URL_Length())
         self.features.append(self.Shortining_Service())
@@ -18,6 +51,8 @@ class FeaturesExtractor:
         self.features.append(self.Prefix_Suffix())
         self.features.append(self.having_Sub_Domain())
         self.features.append(self.SSLfinal_State())
+        self.features.append(self.Domain_registeration_length())
+        self.features.append(self.Favicon())
 
     
     def having_IP_Address(self):
@@ -84,25 +119,26 @@ class FeaturesExtractor:
             return -1
         
     def SSLfinal_State(self):
-        protocol = re.split("://",url)[0]
-        if protocol == "https":
-            context = ssl.create_default_context()
-            conn = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname=self.domain)
-            conn.settimeout(3)
-            conn.connect((self.domain, 443))
-            cert = conn.getpeercert()
-            conn.close()
+        try:
+            protocol = self.url_parse.scheme
+            if 'https' in protocol:
+                context = ssl.create_default_context()
+                conn = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname=self.domain)
+                conn.settimeout(3)
+                conn.connect((self.domain, 443))
+                cert = conn.getpeercert()
+                conn.close()
 
-            not_before_str = cert['notBefore']
-            not_after_str = cert['notAfter']
+                not_before_str = cert['notBefore']
+                not_after_str = cert['notAfter']
     
-            not_before = datetime.strptime(not_before_str, "%b %d %H:%M:%S %Y %Z")
-            not_after = datetime.strptime(not_after_str, "%b %d %H:%M:%S %Y %Z")
+                not_before = datetime.strptime(not_before_str, "%b %d %H:%M:%S %Y %Z")
+                not_after = datetime.strptime(not_after_str, "%b %d %H:%M:%S %Y %Z")
 
-            lifetime = not_after.year - not_before.year
+                lifetime = not_after.year - not_before.year
 
-            issuer = cert['issuer']
-            reputable_cas = ["GeoTrust",
+                issuer = cert['issuer']
+                reputable_cas = ["GeoTrust",
                              "GoDaddy",
                              "Network Solutions",
                              "Thawte",
@@ -118,27 +154,49 @@ class FeaturesExtractor:
                              "IdenTrust",
                              "Google"
                              ]
-            issuer = issuer[1][0][1].split(" ")[0]
-            print(issuer)
-            for ca in reputable_cas:
-                if issuer.lower() in ca.lower() and lifetime >= 1:
-                    return 1
-                else:
-                    continue
-            return 0
-        else:
-            return -1
+                issuer = issuer[1][0][1].split(" ")[0]
+                for ca in reputable_cas:
+                    if issuer.lower() in ca.lower() and lifetime >= 1: 
+                        return 1
+                    else:
+                        continue
+                return 0
+            else:
+                return -1
+        except:
+            pass
         
     def Domain_registeration_length(self):
-        pass
+
+        try:
+            expiration_date = self.whois_res.expiration_date[0].year
+            creation_date = self.whois_res.creation_date[0].year
+
+            life = expiration_date - creation_date
+
+            if life <= 1:
+                return -1
+            else:
+                return 1 
+        except:
+            return -1
 
     def Favicon(self):
-        pass
+        try:
+            for head in self.soup.find_all('head'):
+                for head.link in self.soup.find_all('link', href=True):
+                    dots = [x.start(0) for x in re.finditer('\.', head.link['href'])]
+                    if self.url in head.link['href'] or len(dots) == 1 or self.domain in head.link['href']:
+                        return 1
+            return -1
+        except:
+            return -1
 
 
 
 
 
-url = "https://www.instagram.com/"
+url = "https://www.x.com/"
 sample = FeaturesExtractor(url)
+
 print(sample.features)
